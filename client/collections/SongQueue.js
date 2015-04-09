0c7082a53a29a1ba4f3ef569c4548055446ff2c2
Brian <briancjliu@gmail.com>
2015-04-08 17:29:58 -0700

    Completed SongModel, linked view to queue, working on queue functionality

--- client/collections/SongQueue.js
+++ client/collections/SongQueue.js
@@ -2,6 +2,28 @@
 var SongQueue = Songs.extend({
 
   initialize: function(){
-  }
+    this.on('add', function(){
+    if(this.length < 2){
+      this.playFirst()
+    }
+    });
+  },
+
+  playFirst: function(){
+    this.at(0).play();
+  },
+
+  enqueue: function(song){
+    this.add(song);
+    console.log('enqueue was caled');
+    this.playFirst();
+    // if(true){
+    //   this.playFirst();
+    // } 
+  },
+
+  dequeue: function(song){
+    this.remove();
+  },
 
 });
--- client/models/SongModel.js
+++ client/models/SongModel.js
@@ -1,9 +1,32 @@
 // SongModel.js - Defines a backbone model class for songs.
 var SongModel = Backbone.Model.extend({
 
+  defaults: {
+    url: 'default',
+
+    title: 'default',
+
+    artist: 'default'
+  },
+
   play: function(){
     // Triggering an event here will also trigger the event on the collection
     this.trigger('play', this);
-  }
+  },
+
+  enqueue: function(){
+    this.trigger('enqueue', this);
+  },
+
+  dequeue: function(){
+    this.trigger('dequeue', this);
+  },
+
+  ended: function(){
+    // this.trigger();
+    this.trigger('ended', this);
+  },
+
+
 
 });
